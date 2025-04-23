e24a02d1d5f68f5eea16525af15704e5f2268e86
Marcio Dev <79775392+marcio-maker@users.noreply.github.com>
2025-04-23 18:03:45 -0300

    sorvete

---
+++ script.js
@@ -0,0 +1,5 @@
+// Este script é um espaço para funcionalidades extras, se necessário
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Bem-vindo ao site Sou Ice!");
+  });
+  
